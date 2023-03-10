bbaf99e4136bb766b47d8bc8b0675e5b4af40682
Leonardo Zurita <leonardodanielzurita@gmail.com>
2023-03-10 17:35:00 -0300

    primer commit

---
+++ js/primeraClase.js
@@ -0,0 +1,13 @@
+console.log('hola mundo');
+document.write('Hola mundo');
+
+console.log("Este es un 2do mensaje");
+
+const PI = 3.14;
+console.log(typeof PI);
+
+// let nombre = prompt("Escriba un nombre");
+// console.log(nombre);
+
+// nombre = "hola";
+// console.log(nombre);
